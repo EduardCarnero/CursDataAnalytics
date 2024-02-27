75f389b58fafbde7c5110139a50dbc1cffca8832
EduardCarnero <161486430+EduardCarnero@users.noreply.github.com>
2024-02-27 19:51:11 +0100

    Add files via upload

---
+++ Sprint2/sprint2.sql
@@ -0,0 +1,68 @@
+#NIVELL 1
+
+#Ex1
+SELECT transaction.*
+FROM transaction
+LEFT JOIN company ON company.id = transaction.company_id
+WHERE country = 'Germany';
+
+#Ex2
+SELECT DISTINCT(company_name)
+FROM company
+JOIN transaction ON company.id = transaction.company_id
+WHERE amount > (SELECT AVG(amount)
+				FROM transaction);
+                
+#Ex3
+SELECT transaction.*, company_name
+FROM company, transaction
+WHERE company.id = transaction.company_id AND company_name LIKE 'C%';
+
+
+#Ex4
+SELECT company_name
+FROM company
+LEFT JOIN transaction ON company.id = transaction.company_id
+WHERE company_id IS NULL;
+
+#NIVELL 2
+
+#Ex1
+SELECT transaction.*
+FROM transaction, company
+WHERE transaction.company_id = company.id AND country = (SELECT country FROM company WHERE company_name = 'Sed Id Limited');
+
+#Ex2
+SELECT company_name, amount
+FROM company, transaction
+WHERE transaction.company_id = company.id AND amount = (SELECT MAX(amount) FROM transaction);
+
+#NIVELL 3
+
+#Ex1
+SELECT AVG(amount), country
+FROM transaction, company
+WHERE transaction.company_id = company.id
+GROUP BY country
+HAVING AVG(amount) > (SELECT AVG(amount) FROM transaction);
+
+#Ex2
+SELECT company_name, COUNT(transaction.id),
+CASE
+	WHEN COUNT(transaction.id) >= 4 THEN 'TRUE'
+    WHEN COUNT(transaction.id) < 4 THEN 'FALSE'
+    END AS 'Mas de 4 transacciones'
+FROM company, transaction
+WHERE transaction.company_id = company.id
+GROUP BY company_name
+ORDER BY COUNT(transaction.id) DESC;
+
+
+
+
+
+
+
+
+
+
