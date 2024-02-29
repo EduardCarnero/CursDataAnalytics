afc0a4aa0c0865a637d907f57744c5467e42171e
EduardCarnero <161486430+EduardCarnero@users.noreply.github.com>
2024-02-29 13:49:46 +0100

    Add files via upload

---
+++ Sprint4/sprint4.sql
@@ -0,0 +1,39 @@
+
+USE bussines;
+
+#NIVELL 1
+#Ex1
+SELECT COUNT(transaction.id), user.name
+FROM transaction, user
+WHERE amount > (SELECT AVG(amount)
+					FROM transaction)
+GROUP BY user.name
+HAVING COUNT(transaction.id) > 3;
+
+#Ex2
+SELECT (SUM(treansaction.amount)/COUNT(transaction.amount)) AS 'Despesa mitja', company.company_name
+FROM busines.transaction, busines.company
+WHERE company.company_id = transaction.bussines_id
+GROUP BY company_name
+HAVING company_name = 'Donec Ltd.';
+
+#NIVELL 2
+#Ex1
+CREATE TABLE credit_card_states AS
+SELECT
+    credit_card_id,
+    CASE
+        WHEN SUM(declined) = 0 THEN 'active'
+        ELSE 'inactive'
+    END AS state
+FROM (
+    SELECT
+        credit_card_id,
+        CASE
+            WHEN declined = 1 THEN 1
+            ELSE 0
+        END AS declined
+    FROM transactions
+    ORDER BY credit_card_id, transaction_date DESC
+) AS ordered_transactions
+GROUP BY credit_card_id;
